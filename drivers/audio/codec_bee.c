89cb96bc108b661e9ba7f7380effa73d5df475f9
Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>
2025-12-02 14:09:44 +0800

    drivers: codec: support for rtl87x2g
    add codec driver for rtl87x2g
    
    Signed-off-by: Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>

--- drivers/audio/codec_bee.c
+++ drivers/audio/codec_bee.c
@@ -19,12 +19,44 @@
 #include <zephyr/drivers/clock_control/bee_clock_control.h>
 #include <soc.h>
 
+#if defined(CONFIG_SOC_SERIES_RTL87X2G)
+#include <rtl_rcc.h>
+#include <rtl_codec.h>
+#elif defined(CONFIG_SOC_SERIES_RTL8752H)
 #include <rtl876x_rcc.h>
 #include <rtl876x_codec.h>
+#endif
 
 #include <zephyr/logging/log.h>
 
 #include "trace.h"
+
+#if defined(CONFIG_SOC_SERIES_RTL87X2G)
+#define BEE_CODEC_CH_AMIC              CODEC_CH_AMIC
+#define BEE_CODEC_CH_DMIC              CODEC_CH_DMIC
+#define BEE_DMIC_Ch_Falling_Latch      DMIC_Ch_Falling_Latch
+#define BEE_DMIC_Ch_Rising_Latch       DMIC_Ch_Rising_Latch
+#define BEE_CODEC_I2SDataWidth         CODEC_I2SRxDataWidth
+#define BEE_CODEC_I2S_DataWidth_8Bits  CODEC_I2S_Rx_DataWidth_8Bits
+#define BEE_CODEC_I2S_DataWidth_16Bits CODEC_I2S_Rx_DataWidth_16Bits
+#define BEE_CODEC_ADC_SampleRate       CODEC_SampleRate0
+#define BEE_CODEC_DAC_SampleRate       CODEC_SampleRate1
+#define BEE_CODEC_MUTE                 CODEC_MUTE
+#define BEE_CODEC_UNMUTE               CODEC_UNMUTE
+#elif defined(CONFIG_SOC_SERIES_RTL8752H)
+#define BEE_CODEC_CH_AMIC              CODEC_CH0_AMIC
+#define BEE_CODEC_CH_DMIC              CODEC_CH0_DMIC
+#define BEE_DMIC_Ch_Falling_Latch      DMIC_Ch0_Falling_Latch
+#define BEE_DMIC_Ch_Rising_Latch       DMIC_Ch0_Rising_Latch
+#define BEE_CODEC_I2SDataWidth         CODEC_I2SDataWidth
+#define BEE_CODEC_I2S_DataWidth_8Bits  CODEC_I2S_DataWidth_8Bits
+#define BEE_CODEC_I2S_DataWidth_16Bits CODEC_I2S_DataWidth_16Bits
+#define BEE_CODEC_I2S_DataWidth_24Bits CODEC_I2S_DataWidth_24Bits
+#define BEE_CODEC_ADC_SampleRate       CODEC_SampleRate
+#define BEE_CODEC_MUTE                 CODEC_CH0_MUTE
+#define BEE_CODEC_UNMUTE               CODEC_CH0_UNMUTE
+
+#endif
 
 #define DT_DRV_COMPAT realtek_bee_codec
 
@@ -40,53 +72,6 @@ struct codec_bee_data {
 	struct i2s_config i2s_cfg;
 	size_t pcm_mem_size;
 	struct k_mem_slab *pcm_mem_slab;
-};
-
-enum codec_channel_sequence_e {
-	CODEC_SEQ_L_R,
-	CODEC_SEQ_R_L,
-	CODEC_SEQ_L_L,
-	CODEC_SEQ_R_R,
-};
-
-enum codec_mic_bias_e {
-	CODEC_MIC_BIAS_1_507,
-	CODEC_MIC_BIAS_1_62,
-	CODEC_MIC_BIAS_1_705,
-	CODEC_MIC_BIAS_1_8,
-	CODEC_MIC_BIAS_1_906,
-	CODEC_MIC_BIAS_2_025,
-	CODEC_MIC_BIAS_2_16,
-	CODEC_MIC_BIAS_2_314,
-};
-
-enum codec_mic_bst_gain_e {
-	CODEC_MIC_BST_GAIN_0DB,
-	CODEC_MIC_BST_GAIN_20DB,
-	CODEC_MIC_BST_GAIN_30DB,
-	CODEC_MIC_BST_GAIN_40DB,
-};
-
-enum codec_mic_bst_mode_e {
-	CODEC_MIC_BST_MODE_SINGLE,
-	CODEC_MIC_BST_MODE_DIFF,
-};
-
-enum codec_mic_type_e {
-	CODEC_MIC_TYPE_AMIC,
-	CODEC_MIC_TYPE_DMIC,
-};
-
-enum codec_dmic_data_latch_e {
-	CODEC_DMIC_DATA_LATCH_FALLING,
-	CODEC_DMIC_DATA_LATCH_RISING,
-};
-
-enum codec_boost_gain_e {
-	CODEC_BOOST_GAIN_0DB,
-	CODEC_BOOST_GAIN_12DB,
-	CODEC_BOOST_GAIN_24DB,
-	CODEC_BOOST_GAIN_36DB,
 };
 
 static const uint32_t codec_channel_sequence_table[] = {
@@ -114,28 +99,30 @@ static const uint32_t codec_mic_bst_mode_table[] = {
 };
 
 static const uint32_t codec_mic_type_table[] = {
-	CODEC_CH0_AMIC,
-	CODEC_CH0_DMIC,
+	BEE_CODEC_CH_AMIC,
+	BEE_CODEC_CH_DMIC,
 };
 
 static const uint32_t codec_dmic_data_latch_table[] = {
-	DMIC_Ch0_Falling_Latch,
-	DMIC_Ch0_Rising_Latch,
+	BEE_DMIC_Ch_Falling_Latch,
+	BEE_DMIC_Ch_Rising_Latch,
 };
 
+#if defined(CONFIG_SOC_SERIES_RTL8752H)
 static const uint32_t codec_boost_gain_table[] = {
 	Ch0_Boost_Gain_0dB,
 	Ch0_Boost_Gain_12dB,
 	Ch0_Boost_Gain_24dB,
 	Ch0_Boost_Gain_36dB,
 };
+#endif
 
 static int codec_bee_configure(const struct device *dev, struct audio_codec_cfg *cfg)
 {
 	struct codec_bee_data *data = dev->data;
 	struct i2s_config *i2s_cfg = &cfg->dai_cfg.i2s;
 
-	if (cfg->dai_type != AUDIO_DAI_TYPE_I2S) {
+	if (cfg->dai_type != AUDIO_DAI_TYPE_I2S || !i2s_cfg) {
 		return -EINVAL;
 	}
 
@@ -169,21 +156,23 @@ static int codec_bee_configure(const struct device *dev, struct audio_codec_cfg 
 	}
 
 	if (i2s_cfg->word_size == 8) {
-		data->codec_initstruct.CODEC_I2SDataWidth = CODEC_I2S_DataWidth_8Bits;
+		data->codec_initstruct.BEE_CODEC_I2SDataWidth = BEE_CODEC_I2S_DataWidth_8Bits;
 	} else if (i2s_cfg->word_size == 16) {
-		data->codec_initstruct.CODEC_I2SDataWidth = CODEC_I2S_DataWidth_16Bits;
+		data->codec_initstruct.BEE_CODEC_I2SDataWidth = BEE_CODEC_I2S_DataWidth_16Bits;
+#if defined(CONFIG_SOC_SERIES_RTL8752H)
 	} else if (i2s_cfg->word_size == 24) {
-		data->codec_initstruct.CODEC_I2SDataWidth = CODEC_I2S_DataWidth_24Bits;
+		data->codec_initstruct.BEE_CODEC_I2SDataWidth = BEE_CODEC_I2S_DataWidth_24Bits;
+#endif
 	} else {
 		return -EINVAL;
 	}
 
 	if (i2s_cfg->frame_clk_freq == 8000) {
-		data->codec_initstruct.CODEC_SampleRate = SAMPLE_RATE_8KHz;
+		data->codec_initstruct.BEE_CODEC_ADC_SampleRate = SAMPLE_RATE_8KHz;
 	}
 
 	if (i2s_cfg->frame_clk_freq == 16000) {
-		data->codec_initstruct.CODEC_SampleRate = SAMPLE_RATE_16KHz;
+		data->codec_initstruct.BEE_CODEC_ADC_SampleRate = SAMPLE_RATE_16KHz;
 	} else {
 		return -EINVAL;
 	}
@@ -196,15 +185,39 @@ static int codec_bee_set_property(const struct device *dev, audio_property_t pro
 {
 	struct codec_bee_data *data = dev->data;
 
-	if (property != AUDIO_PROPERTY_OUTPUT_MUTE) {
-		return -EINVAL;
-	}
-
 	if (channel != AUDIO_CHANNEL_ALL) {
 		return -EINVAL;
 	}
 
-	data->codec_initstruct.CODEC_Ch0Mute = val.mute ? CODEC_CH0_MUTE : CODEC_CH0_UNMUTE;
+	if (property == AUDIO_PROPERTY_OUTPUT_MUTE) {
+#if defined(CONFIG_SOC_SERIES_RTL87X2G)
+		data->codec_initstruct.CODEC_Ch0Mute = val.mute ? BEE_CODEC_MUTE : BEE_CODEC_UNMUTE;
+		if (data->codec_initstruct.CODEC_Ch1MicType == BEE_CODEC_CH_DMIC &&
+		    data->i2s_cfg.channels == 2) {
+			data->codec_initstruct.CODEC_Ch1Mute =
+				val.mute ? BEE_CODEC_MUTE : BEE_CODEC_UNMUTE;
+		} else if (data->i2s_cfg.channels == 0) {
+			data->codec_initstruct.CODEC_DaMute =
+				val.mute ? BEE_CODEC_MUTE : BEE_CODEC_UNMUTE;
+		}
+#elif defined(CONFIG_SOC_SERIES_RTL8752H)
+		data->codec_initstruct.CODEC_Ch0Mute = val.mute ? BEE_CODEC_MUTE : BEE_CODEC_UNMUTE;
+#endif
+	} else if (property == AUDIO_PROPERTY_OUTPUT_VOLUME) {
+#if defined(CONFIG_SOC_SERIES_RTL87X2G)
+		data->codec_initstruct.CODEC_Ch0Mute = val.mute ? BEE_CODEC_MUTE : BEE_CODEC_UNMUTE;
+		if (data->codec_initstruct.CODEC_Ch1MicType == BEE_CODEC_CH_DMIC &&
+		    data->i2s_cfg.channels == 2) {
+			data->codec_initstruct.CODEC_Ch1AdGain = val.vol;
+		} else if (data->i2s_cfg.channels == 0) {
+			data->codec_initstruct.CODEC_DaGain = val.vol;
+		}
+#elif defined(CONFIG_SOC_SERIES_RTL8752H)
+		data->codec_initstruct.CODEC_Ch0AdGain = val.vol;
+#endif
+	} else {
+		return -EINVAL;
+	}
 
 	return 0;
 }
@@ -259,6 +272,69 @@ static int codec_bee_init(const struct device *dev)
 	return 0;
 }
 
+#if defined(CONFIG_SOC_SERIES_RTL87X2G)
+#define BEE_CODEC_STRUCT_INIT(index)                                                               \
+	.codec_initstruct = {                                                                      \
+		.CODEC_AdSampleRateSrc = SAMPLE_RATE_SRC0,                                         \
+		.CODEC_DaSampleRateSrc = SAMPLE_RATE_SRC1,                                         \
+		.BEE_CODEC_ADC_SampleRate = SAMPLE_RATE_16KHz,                                     \
+		.BEE_CODEC_DAC_SampleRate = SAMPLE_RATE_16KHz,                                     \
+		.CODEC_I2SFormat = CODEC_I2S_DataFormat_I2S,                                       \
+		.BEE_CODEC_I2SDataWidth = BEE_CODEC_I2S_DataWidth_16Bits,                          \
+		.CODEC_I2STxDataWidth = BEE_CODEC_I2S_DataWidth_16Bits,                            \
+		.CODEC_I2SSameLrcEn = DISABLE,                                                     \
+		.CODEC_I2SChSequence = codec_channel_sequence_table[DT_INST_ENUM_IDX_OR(           \
+			index, channel_sequence, CODEC_I2S_CH_L_R)],                               \
+		.CODEC_MicBIAS = codec_mic_bias_table[DT_INST_ENUM_IDX_OR(index, mic_bias,         \
+									  MICBIAS_VOLTAGE_1_8)],   \
+		.CODEC_MicBstMode = codec_mic_bst_mode_table[DT_INST_ENUM_IDX_OR(                  \
+			index, mic_bst_mode, MICBST_Mode_Single)],                                 \
+		.CODEC_MicBstGain = codec_mic_bst_gain_table[DT_INST_ENUM_IDX_OR(                  \
+			index, mic_bst_gain, MICBST_Gain_20dB)],                                   \
+		.CODEC_DmicClock = DMIC_Clock_2500KHz,                                             \
+		.CODEC_Ch0MicType = codec_mic_type_table[DT_INST_ENUM_IDX_OR(index, mic_type,      \
+									     BEE_CODEC_CH_AMIC)],  \
+		.CODEC_Ch0Mute = BEE_CODEC_MUTE,                                                   \
+		.CODEC_Ch0DmicDataLatch = codec_dmic_data_latch_table[DT_INST_ENUM_IDX_OR(         \
+			index, dmic_data_latch, DMIC_Ch0_Rising_Latch)],                           \
+		.CODEC_Ch0AdGain = DT_INST_PROP_OR(index, ad_gain, 0x2f),                          \
+		.CODEC_Ch1MicType = codec_mic_type_table[DT_INST_ENUM_IDX_OR(index, mic_type,      \
+									     BEE_CODEC_CH_AMIC)],  \
+		.CODEC_Ch1Mute = BEE_CODEC_MUTE,                                                   \
+		.CODEC_Ch1DmicDataLatch = codec_dmic_data_latch_table[DT_INST_ENUM_IDX_OR(         \
+			index, dmic_data_latch, DMIC_Ch0_Rising_Latch)],                           \
+		.CODEC_Ch1AdGain = DT_INST_PROP_OR(index, ad_gain, 0x2f),                          \
+		.CODEC_DaMute = BEE_CODEC_MUTE,                                                    \
+		.CODEC_DaGain = DT_INST_PROP_OR(index, da_gain, 0xAf),                             \
+		.CODEC_DaC_Dither = DAC_DA_DITHER_DISABLE,                                         \
+		.CODEC_I2SChannelLen = I2S_CHANNELLEN_32,                                          \
+	},
+#elif defined(CONFIG_SOC_SERIES_RTL8752H)
+#define BEE_CODEC_STRUCT_INIT(index)                                                               \
+	.codec_initstruct = {                                                                      \
+		.BEE_CODEC_ADC_SampleRate = SAMPLE_RATE_16KHz,                                     \
+		.CODEC_I2SFormat = CODEC_I2S_DataFormat_I2S,                                       \
+		.CODEC_I2SDataWidth = BEE_CODEC_I2S_DataWidth_16Bits,                              \
+		.CODEC_I2SChSequence = codec_channel_sequence_table[DT_INST_ENUM_IDX_OR(           \
+			index, channel_sequence, CODEC_I2S_CH_L_L)],                               \
+		.CODEC_MicBIAS = codec_mic_bias_table[DT_INST_ENUM_IDX_OR(index, mic_bias,         \
+									  MICBIAS_VOLTAGE_1_8)],   \
+		.CODEC_MicBstMode = codec_mic_bst_mode_table[DT_INST_ENUM_IDX_OR(                  \
+			index, mic_bst_mode, MICBST_Mode_Single)],                                 \
+		.CODEC_MicBstGain = codec_mic_bst_gain_table[DT_INST_ENUM_IDX_OR(                  \
+			index, mic_bst_gain, MICBST_Gain_0dB)],                                    \
+		.CODEC_DmicClock = DMIC_Clock_2500KHz,                                             \
+		.CODEC_Ch0MicType = codec_mic_type_table[DT_INST_ENUM_IDX_OR(index, mic_type,      \
+									     BEE_CODEC_CH_AMIC)],  \
+		.CODEC_Ch0BoostGain = codec_boost_gain_table[DT_INST_ENUM_IDX_OR(                  \
+			index, boost_gain, Ch0_Boost_Gain_0dB)],                                   \
+		.CODEC_Ch0Mute = BEE_CODEC_UNMUTE,                                                 \
+		.CODEC_Ch0DmicDataLatch = codec_dmic_data_latch_table[DT_INST_ENUM_IDX_OR(         \
+			index, dmic_data_latch, DMIC_Ch0_Rising_Latch)],                           \
+		.CODEC_Ch0AdGain = DT_INST_PROP_OR(index, ad_gain, 0x2f),                          \
+	},
+#endif
+
 #define BEE_CODEC_INIT(index)                                                                      \
 	PINCTRL_DT_INST_DEFINE(index);                                                             \
                                                                                                    \
@@ -267,34 +343,7 @@ static int codec_bee_init(const struct device *dev)
 		.pinctrl = PINCTRL_DT_INST_DEV_CONFIG_GET(index),                                  \
 	};                                                                                         \
                                                                                                    \
-	static struct codec_bee_data codec_bee_data_##index = {                                    \
-		.codec_initstruct =                                                                \
-			{                                                                          \
-				.CODEC_SampleRate = SAMPLE_RATE_16KHz,                             \
-				.CODEC_I2SFormat = CODEC_I2S_DataFormat_I2S,                       \
-				.CODEC_I2SDataWidth = CODEC_I2S_DataWidth_16Bits,                  \
-				.CODEC_I2SChSequence =                                             \
-					codec_channel_sequence_table[DT_INST_ENUM_IDX_OR(          \
-						index, channel_sequence, CODEC_I2S_CH_L_L)],       \
-				.CODEC_MicBIAS = codec_mic_bias_table[DT_INST_ENUM_IDX_OR(         \
-					index, mic_bias, MICBIAS_VOLTAGE_1_8)],                    \
-				.CODEC_MicBstMode = codec_mic_bst_mode_table[DT_INST_ENUM_IDX_OR(  \
-					index, mic_bst_mode, MICBST_Mode_Single)],                 \
-				.CODEC_MicBstGain = codec_mic_bst_gain_table[DT_INST_ENUM_IDX_OR(  \
-					index, mic_bst_gain, MICBST_Gain_0dB)],                    \
-				.CODEC_DmicClock = DMIC_Clock_2500KHz,                             \
-				.CODEC_Ch0MicType = codec_mic_type_table[DT_INST_ENUM_IDX_OR(      \
-					index, mic_type, CODEC_CH0_AMIC)],                         \
-				.CODEC_Ch0BoostGain = codec_boost_gain_table[DT_INST_ENUM_IDX_OR(  \
-					index, boost_gain, Ch0_Boost_Gain_0dB)],                   \
-				.CODEC_Ch0Mute = CODEC_CH0_UNMUTE,                                 \
-				.CODEC_Ch0ZeroDetTimeout = Ch0_ADC_Zero_DetTimeout_1024_16_Sample, \
-				.CODEC_Ch0DmicDataLatch =                                          \
-					codec_dmic_data_latch_table[DT_INST_ENUM_IDX_OR(           \
-						index, dmic_data_latch, DMIC_Ch0_Rising_Latch)],   \
-				.CODEC_Ch0AdGain = DT_INST_PROP_OR(index, ad_gain, 0x2f),          \
-			},                                                                         \
-	};                                                                                         \
+	static struct codec_bee_data codec_bee_data_##index = {BEE_CODEC_STRUCT_INIT(index)};      \
                                                                                                    \
 	DEVICE_DT_INST_DEFINE(index, &codec_bee_init, NULL, &codec_bee_data_##index,               \
 			      &codec_bee_cfg_##index, POST_KERNEL,                                 \
