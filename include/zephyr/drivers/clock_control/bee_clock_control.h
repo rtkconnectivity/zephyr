f2c336ae80eb8e768c7d898c1599726c846f10a4
Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>
2026-01-21 09:57:47 +0800

    drivers: clock: add realtek bee clock driver
    Add realtek bee clock driver.
    
    Signed-off-by: Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>

---
+++ include/zephyr/drivers/clock_control/bee_clock_control.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026, Realtek Semiconductor Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_BEE_H_
+#define ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_BEE_H_
+
+#include <zephyr/device.h>
+
+#define BEE_CLOCK_CONTROLLER DEVICE_DT_GET(DT_NODELABEL(cctl))
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_BEE_H_ */
