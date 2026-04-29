b247e78f31faa49eae6922d070b667ca84f5579f
Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>
2026-04-29 15:22:51 +0800

    drivers: counter: add rtl87x2j counter timer driver
    Add rtl87x2j series in bee counter timer driver.
    
    Signed-off-by: Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>

--- soc/realtek/bee/common/bee_timer_common.h
+++ soc/realtek/bee/common/bee_timer_common.h
@@ -27,6 +27,8 @@
 #include <rtl876x_rcc.h>
 #include <rtl876x_nvic.h>
 #include <vector_table.h>
+#elif defined(CONFIG_SOC_SERIES_RTL87X2J)
+#include <rtl_timer.h>
 #else
 #error "Unsupported Realtek Bee SoC series"
 #endif
