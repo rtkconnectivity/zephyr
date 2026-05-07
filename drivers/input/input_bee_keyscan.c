67e9bdac5405ac720c2a4169073712ca608de0ee
Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>
2026-05-07 09:50:30 +0800

    drivers: input: add rtl87x2j keyscan driver
    Add rtl87x2j series in bee keyscan driver.
    
    Signed-off-by: Yuzhuo Liu <yuzhuo_liu@realsil.com.cn>

--- drivers/input/input_bee_keyscan.c
+++ drivers/input/input_bee_keyscan.c
@@ -31,6 +31,9 @@ LOG_MODULE_REGISTER(bee_keyscan, CONFIG_INPUT_LOG_LEVEL);
 #include "rtl876x_rcc.h"
 #include "rtl876x_pinmux.h"
 #include "vector_table.h"
+#elif defined(CONFIG_SOC_SERIES_RTL87X2J)
+#include "rtl_keyscan.h"
+#include "rtl_pinmux.h"
 #else
 #error "Unsupported Realtek Bee SoC series"
 #endif
@@ -46,11 +49,122 @@ LOG_MODULE_REGISTER(bee_keyscan, CONFIG_INPUT_LOG_LEVEL);
 #define BEE_KEYSCAN_PRE_GUARD_MASK        GENMASK(28, 26)
 
 #if defined(CONFIG_SOC_SERIES_RTL87X2G)
+#define BEE_KEYSCAN_STRUCT_INIT(init_struct)          KeyScan_StructInit(init_struct)
+#define BEE_KEYSCAN_INIT(kscan, init_struct)          KeyScan_Init(kscan, init_struct)
+#define BEE_KEYSCAN_INT_CONFIG(kscan, int, cmd)       KeyScan_INTConfig(kscan, int, cmd)
+#define BEE_KEYSCAN_CLEAR_INT_PENDING_BIT(kscan, int) KeyScan_ClearINTPendingBit(kscan, int)
+#define BEE_KEYSCAN_INT_MASK(kscan, int, cmd)         KeyScan_INTMask(kscan, int, cmd)
+#define BEE_KEYSCAN_CMD(kscan, cmd)                   KeyScan_Cmd(kscan, cmd)
+#define BEE_KEYSCAN_GET_FIFO_DATA_NUM(kscan)          KeyScan_GetFifoDataNum(kscan)
+#define BEE_KEYSCAN_GET_FLAG_STATE(kscan, flag)       KeyScan_GetFlagState(kscan, flag)
+#define BEE_KEYSCAN_READ(kscan, buf, num)             KeyScan_Read(kscan, buf, num)
+
+#define BEE_KEYSCAN_ROW_SIZE        rowSize
+#define BEE_KEYSCAN_COL_SIZE        colSize
+#define BEE_KEYSCAN_CLOCK_DIV       clockdiv
+#define BEE_KEYSCAN_CLOCK_DELAY_DIV delayclk
+#define BEE_KEYSCAN_DEBOUNCE_EN     debounceEn
+#define BEE_KEYSCAN_SCAN_TIMER_EN   scantimerEn
+#define BEE_KEYSCAN_DETECT_TIMER_EN detecttimerEn
+#define BEE_KEYSCAN_DEBOUNCE_CNT    debouncecnt
+#define BEE_KEYSCAN_SCAN_INTERVAL   scanInterval
+#define BEE_KEYSCAN_RELEASE_CNT     releasecnt
+#define BEE_KEYSCAN_MANUAL_SEL      manual_sel
+#define BEE_KEYSCAN_SCAN_MODE       scanmode
+#define BEE_KEYSCAN_KEY_LIMIT       keylimit
+
+#define BEE_KEYSCAN_AUTO_SCAN_MODE   KeyScan_Auto_Scan_Mode
+#define BEE_KEYSCAN_MANUAL_SCAN_MODE KeyScan_Manual_Scan_Mode
+#define BEE_KEYSCAN_MANUAL_SEL_KEY   KeyScan_Manual_Sel_Key
+#define BEE_KEYSCAN_MANUAL_SEL_BIT   KeyScan_Manual_Sel_Bit
+
+#define BEE_KEYSCAN_DEB_EN           ENABLE
+#define BEE_KEYSCAN_DEB_DIS          DISABLE
+#define BEE_KEYSCAN_SCANINTERVAL_EN  ENABLE
+#define BEE_KEYSCAN_SCANINTERVAL_DIS DISABLE
+#define BEE_KEYSCAN_DETECT_EN        ENABLE
+#define BEE_KEYSCAN_DETECT_DIS       DISABLE
+
 #define BEE_KEYSCAN_REG_CLKDIV KEYSCAN_CLK_DIV
 #define BEE_KEYSCAN_FIFO_DEPTH KEYSCAN_FIFO_DEPTH
 #elif defined(CONFIG_SOC_SERIES_RTL8752H)
+#define BEE_KEYSCAN_STRUCT_INIT(init_struct)          KeyScan_StructInit(init_struct)
+#define BEE_KEYSCAN_INIT(kscan, init_struct)          KeyScan_Init(kscan, init_struct)
+#define BEE_KEYSCAN_INT_CONFIG(kscan, int, cmd)       KeyScan_INTConfig(kscan, int, cmd)
+#define BEE_KEYSCAN_CLEAR_INT_PENDING_BIT(kscan, int) KeyScan_ClearINTPendingBit(kscan, int)
+#define BEE_KEYSCAN_INT_MASK(kscan, int, cmd)         KeyScan_INTMask(kscan, int, cmd)
+#define BEE_KEYSCAN_CMD(kscan, cmd)                   KeyScan_Cmd(kscan, cmd)
+#define BEE_KEYSCAN_GET_FIFO_DATA_NUM(kscan)          KeyScan_GetFifoDataNum(kscan)
+#define BEE_KEYSCAN_GET_FLAG_STATE(kscan, flag)       KeyScan_GetFlagState(kscan, flag)
+#define BEE_KEYSCAN_READ(kscan, buf, num)             KeyScan_Read(kscan, buf, num)
+
+#define BEE_KEYSCAN_ROW_SIZE        rowSize
+#define BEE_KEYSCAN_COL_SIZE        colSize
+#define BEE_KEYSCAN_CLOCK_DIV       clockdiv
+#define BEE_KEYSCAN_CLOCK_DELAY_DIV delayclk
+#define BEE_KEYSCAN_DEBOUNCE_EN     debounceEn
+#define BEE_KEYSCAN_SCAN_TIMER_EN   scantimerEn
+#define BEE_KEYSCAN_DETECT_TIMER_EN detecttimerEn
+#define BEE_KEYSCAN_DEBOUNCE_CNT    debouncecnt
+#define BEE_KEYSCAN_SCAN_INTERVAL   scanInterval
+#define BEE_KEYSCAN_RELEASE_CNT     releasecnt
+#define BEE_KEYSCAN_MANUAL_SEL      manual_sel
+#define BEE_KEYSCAN_SCAN_MODE       scanmode
+#define BEE_KEYSCAN_KEY_LIMIT       keylimit
+
+#define BEE_KEYSCAN_AUTO_SCAN_MODE   KeyScan_Auto_Scan_Mode
+#define BEE_KEYSCAN_MANUAL_SCAN_MODE KeyScan_Manual_Scan_Mode
+#define BEE_KEYSCAN_MANUAL_SEL_KEY   KeyScan_Manual_Sel_Key
+#define BEE_KEYSCAN_MANUAL_SEL_BIT   KeyScan_Manual_Sel_Bit
+
+#define BEE_KEYSCAN_DEB_EN           KeyScan_Debounce_Enable
+#define BEE_KEYSCAN_DEB_DIS          KeyScan_Debounce_Disable
+#define BEE_KEYSCAN_SCANINTERVAL_EN  KeyScan_ScanInterval_Enable
+#define BEE_KEYSCAN_SCANINTERVAL_DIS KeyScan_ScanInterval_Disable
+#define BEE_KEYSCAN_DETECT_EN        KeyScan_Release_Detect_Enable
+#define BEE_KEYSCAN_DETECT_DIS       KeyScan_Release_Detect_Disable
+
 #define BEE_KEYSCAN_REG_CLKDIV CLKDIV
 #define BEE_KEYSCAN_FIFO_DEPTH 26
+#elif defined(CONFIG_SOC_SERIES_RTL87X2J)
+#define BEE_KEYSCAN_STRUCT_INIT(init_struct)          KEYSCAN_StructInit(init_struct)
+#define BEE_KEYSCAN_INIT(kscan, init_struct)          KEYSCAN_Init(kscan, init_struct)
+#define BEE_KEYSCAN_INT_CONFIG(kscan, int, cmd)       KEYSCAN_INTConfig(kscan, int, cmd)
+#define BEE_KEYSCAN_CLEAR_INT_PENDING_BIT(kscan, int) KEYSCAN_ClearINTPendingBit(kscan, int)
+#define BEE_KEYSCAN_INT_MASK(kscan, int, cmd)         KEYSCAN_INTMask(kscan, int, cmd)
+#define BEE_KEYSCAN_CMD(kscan, cmd)                   KEYSCAN_Cmd(kscan, cmd)
+#define BEE_KEYSCAN_GET_FIFO_DATA_NUM(kscan)          KEYSCAN_GetFIFODataNum(kscan)
+#define BEE_KEYSCAN_GET_FLAG_STATE(kscan, flag)       KEYSCAN_GetFlagState(kscan, flag)
+#define BEE_KEYSCAN_READ(kscan, buf, num)             KEYSCAN_Read(kscan, buf, num)
+
+#define BEE_KEYSCAN_ROW_SIZE        KEYSCAN_RowSize
+#define BEE_KEYSCAN_COL_SIZE        KEYSCAN_ColSize
+#define BEE_KEYSCAN_CLOCK_DIV       KEYSCAN_ClockDiv
+#define BEE_KEYSCAN_CLOCK_DELAY_DIV KEYSCAN_ClockDelayDiv
+#define BEE_KEYSCAN_DEBOUNCE_EN     KEYSCAN_DebounceEn
+#define BEE_KEYSCAN_SCAN_TIMER_EN   KEYSCAN_ScanTimerEn
+#define BEE_KEYSCAN_DETECT_TIMER_EN KEYSCAN_DetectTimerEn
+#define BEE_KEYSCAN_DEBOUNCE_CNT    KEYSCAN_DebounceCnt
+#define BEE_KEYSCAN_SCAN_INTERVAL   KEYSCAN_ScanInterval
+#define BEE_KEYSCAN_RELEASE_CNT     KEYSCAN_ReleaseCnt
+#define BEE_KEYSCAN_MANUAL_SEL      KEYSCAN_ManualSel
+#define BEE_KEYSCAN_SCAN_MODE       KEYSCAN_ScanMode
+#define BEE_KEYSCAN_KEY_LIMIT       KEYSCAN_KeyLimit
+
+#define BEE_KEYSCAN_AUTO_SCAN_MODE   KEYSCAN_AUTO_SCAN_MODE
+#define BEE_KEYSCAN_MANUAL_SCAN_MODE KEYSCAN_MANUAL_SCAN_MODE
+#define BEE_KEYSCAN_MANUAL_SEL_KEY   KEYSCAN_MANUAL_SEL_KEY
+#define BEE_KEYSCAN_MANUAL_SEL_BIT   KEYSCAN_MANUAL_SEL_BIT
+
+#define BEE_KEYSCAN_DEB_EN           ENABLE
+#define BEE_KEYSCAN_DEB_DIS          DISABLE
+#define BEE_KEYSCAN_SCANINTERVAL_EN  ENABLE
+#define BEE_KEYSCAN_SCANINTERVAL_DIS DISABLE
+#define BEE_KEYSCAN_DETECT_EN        ENABLE
+#define BEE_KEYSCAN_DETECT_DIS       DISABLE
+
+#define BEE_KEYSCAN_REG_CLKDIV KEYSCAN_CLK_DIV
+#define BEE_KEYSCAN_FIFO_DEPTH KEYSCAN_FIFO_DEPTH
 #endif
 
 struct keyscan_key_index {
@@ -96,49 +210,43 @@ static int bee_keyscan_init_driver(const struct device *dev, uint32_t scanmode, 
 	KEYSCAN_TypeDef *keyscan = config->reg;
 	KEYSCAN_InitTypeDef keyscan_init_struct;
 
-	KeyScan_StructInit(&keyscan_init_struct);
-	keyscan_init_struct.rowSize = config->common.row_size;
-	keyscan_init_struct.colSize = config->common.col_size;
+	BEE_KEYSCAN_STRUCT_INIT(&keyscan_init_struct);
+	keyscan_init_struct.BEE_KEYSCAN_ROW_SIZE = config->common.row_size;
+	keyscan_init_struct.BEE_KEYSCAN_COL_SIZE = config->common.col_size;
 
-	keyscan_init_struct.clockdiv = config->scan_div;
-	keyscan_init_struct.delayclk = config->delay_div;
+	keyscan_init_struct.BEE_KEYSCAN_CLOCK_DIV = config->scan_div;
+	keyscan_init_struct.BEE_KEYSCAN_CLOCK_DELAY_DIV = config->delay_div;
 
-	keyscan_init_struct.scanInterval = config->scan_cnt;
-	keyscan_init_struct.releasecnt = config->rel_cnt;
+	keyscan_init_struct.BEE_KEYSCAN_SCAN_INTERVAL = config->scan_cnt;
+	keyscan_init_struct.BEE_KEYSCAN_RELEASE_CNT = config->rel_cnt;
 
-#if defined(CONFIG_SOC_SERIES_RTL87X2G)
-	keyscan_init_struct.debounceEn = DISABLE;
-	keyscan_init_struct.scantimerEn = keyscan_init_struct.scanInterval ? ENABLE : DISABLE;
-	keyscan_init_struct.detecttimerEn = keyscan_init_struct.releasecnt ? ENABLE : DISABLE;
-#elif defined(CONFIG_SOC_SERIES_RTL8752H)
-	keyscan_init_struct.debounceEn = KeyScan_Debounce_Disable;
-	keyscan_init_struct.scantimerEn = keyscan_init_struct.scanInterval
-						  ? KeyScan_ScanInterval_Enable
-						  : KeyScan_ScanInterval_Disable;
-	keyscan_init_struct.detecttimerEn = keyscan_init_struct.releasecnt
-						    ? KeyScan_Release_Detect_Enable
-						    : KeyScan_Release_Detect_Disable;
-#endif
+	keyscan_init_struct.BEE_KEYSCAN_DEBOUNCE_EN = BEE_KEYSCAN_DEB_DIS;
+	keyscan_init_struct.BEE_KEYSCAN_SCAN_TIMER_EN =
+		keyscan_init_struct.BEE_KEYSCAN_SCAN_INTERVAL ? BEE_KEYSCAN_SCANINTERVAL_EN
+							      : BEE_KEYSCAN_SCANINTERVAL_DIS;
+	keyscan_init_struct.BEE_KEYSCAN_DETECT_TIMER_EN =
+		keyscan_init_struct.BEE_KEYSCAN_RELEASE_CNT ? BEE_KEYSCAN_DETECT_EN
+							    : BEE_KEYSCAN_DETECT_DIS;
 
-	keyscan_init_struct.manual_sel = manual_sel;
-	keyscan_init_struct.scanmode = scanmode;
-	keyscan_init_struct.keylimit = BEE_KEYSCAN_FIFO_DEPTH;
+	keyscan_init_struct.BEE_KEYSCAN_MANUAL_SEL = manual_sel;
+	keyscan_init_struct.BEE_KEYSCAN_SCAN_MODE = scanmode;
+	keyscan_init_struct.BEE_KEYSCAN_KEY_LIMIT = BEE_KEYSCAN_FIFO_DEPTH;
 
-	KeyScan_Init(keyscan, &keyscan_init_struct);
+	BEE_KEYSCAN_INIT(keyscan, &keyscan_init_struct);
 
 	bee_keyscan_set_pre_guard(keyscan, BEE_KEYSCAN_DEFAULT_PRE_GUARD_CNT);
 
-	KeyScan_INTConfig(keyscan, KEYSCAN_INT_SCAN_END, ENABLE);
-	KeyScan_ClearINTPendingBit(keyscan, KEYSCAN_INT_SCAN_END);
-	KeyScan_INTMask(keyscan, KEYSCAN_INT_SCAN_END, DISABLE);
+	BEE_KEYSCAN_INT_CONFIG(keyscan, KEYSCAN_INT_SCAN_END, ENABLE);
+	BEE_KEYSCAN_CLEAR_INT_PENDING_BIT(keyscan, KEYSCAN_INT_SCAN_END);
+	BEE_KEYSCAN_INT_MASK(keyscan, KEYSCAN_INT_SCAN_END, DISABLE);
 
 #if CONFIG_BEE_INPUT_KEYSCAN_AUTOSCAN_MODE
-	KeyScan_INTConfig(keyscan, KEYSCAN_INT_ALL_RELEASE, ENABLE);
-	KeyScan_ClearINTPendingBit(keyscan, KEYSCAN_INT_ALL_RELEASE);
-	KeyScan_INTMask(keyscan, KEYSCAN_INT_ALL_RELEASE, DISABLE);
+	BEE_KEYSCAN_INT_CONFIG(keyscan, KEYSCAN_INT_ALL_RELEASE, ENABLE);
+	BEE_KEYSCAN_CLEAR_INT_PENDING_BIT(keyscan, KEYSCAN_INT_ALL_RELEASE);
+	BEE_KEYSCAN_INT_MASK(keyscan, KEYSCAN_INT_ALL_RELEASE, DISABLE);
 #endif
 
-	KeyScan_Cmd(keyscan, ENABLE);
+	BEE_KEYSCAN_CMD(keyscan, ENABLE);
 
 	return 0;
 }
@@ -155,7 +263,7 @@ static void manual_keyscan_timer_cb(struct k_timer *timer)
 		return;
 	}
 
-	bee_keyscan_init_driver(dev, KeyScan_Manual_Scan_Mode, KeyScan_Manual_Sel_Bit);
+	bee_keyscan_init_driver(dev, BEE_KEYSCAN_MANUAL_SCAN_MODE, BEE_KEYSCAN_MANUAL_SEL_BIT);
 }
 
 static bool
@@ -182,7 +290,7 @@ static void bee_keyscan_process_matrix(const struct device *dev, uint8_t new_pre
 	__maybe_unused KEYSCAN_TypeDef *keyscan = config->reg;
 
 #ifndef CONFIG_BEE_INPUT_KEYSCAN_AUTOSCAN_MODE
-	KeyScan_Cmd(keyscan, DISABLE);
+	BEE_KEYSCAN_CMD(keyscan, DISABLE);
 #endif
 
 	for (int c = 0; c < cfg_common->col_size; c++) {
@@ -213,7 +321,8 @@ static void bee_keyscan_process_matrix(const struct device *dev, uint8_t new_pre
 					(clock_control_subsys_t)&config->clkid);
 		(void)clock_control_on(BEE_CLOCK_CONTROLLER,
 				       (clock_control_subsys_t)&config->clkid);
-		bee_keyscan_init_driver(dev, KeyScan_Manual_Scan_Mode, KeyScan_Manual_Sel_Key);
+		bee_keyscan_init_driver(dev, BEE_KEYSCAN_MANUAL_SCAN_MODE,
+					BEE_KEYSCAN_MANUAL_SEL_KEY);
 		return;
 	}
 #endif
@@ -234,7 +343,7 @@ static void bee_keyscan_work_handler(struct k_work *work)
 
 	bee_keyscan_process_matrix(dev, data->new_press_num, data->new_keys);
 
-	KeyScan_INTMask(keyscan, KEYSCAN_INT_SCAN_END, DISABLE);
+	BEE_KEYSCAN_INT_MASK(keyscan, KEYSCAN_INT_SCAN_END, DISABLE);
 }
 #endif
 
@@ -243,7 +352,7 @@ static void bee_keyscan_isr(const struct device *dev)
 	const struct bee_keyscan_config *config = dev->config;
 	__maybe_unused struct bee_keyscan_data *data = dev->data;
 	KEYSCAN_TypeDef *keyscan = config->reg;
-	uint8_t new_press_num = KeyScan_GetFifoDataNum(keyscan);
+	uint8_t new_press_num = BEE_KEYSCAN_GET_FIFO_DATA_NUM(keyscan);
 
 	if (new_press_num > BEE_KEYSCAN_FIFO_DEPTH) {
 		new_press_num = BEE_KEYSCAN_FIFO_DEPTH;
@@ -251,28 +360,28 @@ static void bee_keyscan_isr(const struct device *dev)
 
 	memset(data->new_keys, 0, sizeof(data->new_keys));
 
-	if (KeyScan_GetFlagState(keyscan, KEYSCAN_INT_FLAG_SCAN_END) == SET) {
-		KeyScan_INTMask(keyscan, KEYSCAN_INT_SCAN_END, ENABLE);
+	if (BEE_KEYSCAN_GET_FLAG_STATE(keyscan, KEYSCAN_INT_FLAG_SCAN_END) == SET) {
+		BEE_KEYSCAN_INT_MASK(keyscan, KEYSCAN_INT_SCAN_END, ENABLE);
 
-		if (KeyScan_GetFlagState(keyscan, KEYSCAN_FLAG_EMPTY) != SET) {
-			KeyScan_Read(keyscan, (uint16_t *)data->new_keys, new_press_num);
+		if (BEE_KEYSCAN_GET_FLAG_STATE(keyscan, KEYSCAN_FLAG_EMPTY) != SET) {
+			BEE_KEYSCAN_READ(keyscan, (uint16_t *)data->new_keys, new_press_num);
 		}
 		data->new_press_num = new_press_num;
 
-		KeyScan_ClearINTPendingBit(keyscan, KEYSCAN_INT_SCAN_END);
+		BEE_KEYSCAN_CLEAR_INT_PENDING_BIT(keyscan, KEYSCAN_INT_SCAN_END);
 
 #if CONFIG_BEE_INPUT_KEYSCAN_AUTOSCAN_MODE
 		bee_keyscan_process_matrix(dev, data->new_press_num, data->new_keys);
-		KeyScan_INTMask(keyscan, KEYSCAN_INT_SCAN_END, DISABLE);
+		BEE_KEYSCAN_INT_MASK(keyscan, KEYSCAN_INT_SCAN_END, DISABLE);
 #else
 		k_work_submit(&data->work);
 #endif
 	}
 
 #if CONFIG_BEE_INPUT_KEYSCAN_AUTOSCAN_MODE
-	if (KeyScan_GetFlagState(keyscan, KEYSCAN_INT_FLAG_ALL_RELEASE) == SET) {
+	if (BEE_KEYSCAN_GET_FLAG_STATE(keyscan, KEYSCAN_INT_FLAG_ALL_RELEASE) == SET) {
 		bee_keyscan_process_matrix(dev, 0, NULL);
-		KeyScan_ClearINTPendingBit(keyscan, KEYSCAN_INT_ALL_RELEASE);
+		BEE_KEYSCAN_CLEAR_INT_PENDING_BIT(keyscan, KEYSCAN_INT_ALL_RELEASE);
 	}
 #endif
 }
@@ -293,9 +402,9 @@ static int bee_keyscan_init(const struct device *dev)
 
 	bee_keyscan_init_driver(dev,
 				IS_ENABLED(CONFIG_BEE_INPUT_KEYSCAN_AUTOSCAN_MODE)
-					? KeyScan_Auto_Scan_Mode
-					: KeyScan_Manual_Scan_Mode,
-				KeyScan_Manual_Sel_Key);
+					? BEE_KEYSCAN_AUTO_SCAN_MODE
+					: BEE_KEYSCAN_MANUAL_SCAN_MODE,
+				BEE_KEYSCAN_MANUAL_SEL_KEY);
 
 #ifndef CONFIG_BEE_INPUT_KEYSCAN_AUTOSCAN_MODE
 	k_work_init(&data->work, bee_keyscan_work_handler);
@@ -307,7 +416,7 @@ static int bee_keyscan_init(const struct device *dev)
 	return 0;
 }
 
-#if defined(CONFIG_SOC_SERIES_RTL87X2G)
+#if defined(CONFIG_SOC_SERIES_RTL87X2G) || defined(CONFIG_SOC_SERIES_RTL87X2J)
 #define BEE_KEYSCAN_IRQ_HANDLER(index)                                                             \
 	static void bee_keyscan_irq_config_func_##index(void)                                      \
 	{                                                                                          \
@@ -350,7 +459,7 @@ static int bee_keyscan_init(const struct device *dev)
 #define BEE_INPUT_KEYSCAN_ASSERT_SCAN_INTERVAL(index)
 #endif
 
-#define BEE_KEYSCAN_INIT(index)                                                                    \
+#define BEE_INPUT_KEYSCAN_INIT(index)                                                              \
 	BUILD_ASSERT(DT_INST_PROP(index, row_size) <= BEE_KEYSCAN_MAX_ROWS,                        \
 		     "DT error: 'row-size' exceeds hardware limit (12)");                          \
 	BUILD_ASSERT(DT_INST_PROP(index, col_size) <= BEE_KEYSCAN_MAX_COLS,                        \
@@ -398,4 +507,4 @@ static int bee_keyscan_init(const struct device *dev)
                                                                                                    \
 	BEE_KEYSCAN_IRQ_HANDLER(index)
 
-DT_INST_FOREACH_STATUS_OKAY(BEE_KEYSCAN_INIT)
+DT_INST_FOREACH_STATUS_OKAY(BEE_INPUT_KEYSCAN_INIT)
