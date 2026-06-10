0ba8ce1545d4f528ca13ab7935297e5ae4b1786c
Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>
2026-06-10 13:46:45 +0800

    soc: rtl87x2j: pm: add debugging code
    add snapshot and low power mode entering count statistics.
    
    Signed-off-by: Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>

--- soc/realtek/bee/rtl87x2j/power.c
+++ soc/realtek/bee/rtl87x2j/power.c
@@ -28,6 +28,63 @@ LOG_MODULE_DECLARE(soc);
 #define BT_POWER_ACTIVE      1
 #endif /* CONFIG_BT */
 
+#ifdef CONFIG_PM_DEBUG_SNAPSHOT
+#include <pck600_snapshot.h>
+
+static const char *const pm_snapshot_ip_names[] = {
+	"I2S",        "ADC",        "UART3",      "UART2",      "UART1",      "UART0",
+	"TMETER",     "TIMER1_CH8", "TIMER1_CH7", "TIMER1_CH6", "TIMER1_CH5", "TIMER1_CH4",
+	"TIMER1_CH3", "TIMER1_CH2", "TIMER1_CH1", "TIMER1_CH0", "TIMER0_CH1", "TIMER0_CH0",
+	"SPI3W",      "SPI2",       "SPI1",       "SPI0",       "KEYSCAN",    "IR",
+	"I2C1",       "I2C0",       "GPIO",       "DMA",        "TIMER2_CH0", "CAN",
+	"USB",        "RFC",        "PRO24G",     "PCC",        "MODEM",      "CAL80M",
+	"CAL32K",     "CAL10M",     "BTMAC",      "BT_VEN",     "AUDIO",      "AACK",
+	"AES",        "SHA256",     "CPU",        "SPIC0",      "RNG",        "VDREG",
+};
+
+static uint32_t pm_wfi_count;
+
+static void pm_snapshot_log_active_ips(void)
+{
+	int key = irq_lock();
+	uint64_t mask = pck600_snapshot_get_device_mask();
+
+	for (int i = 0; i < PCK600_SNAPSHOT_DEVICE_MAX; i++) {
+		if (mask & ((uint64_t)1 << i)) {
+			PCK600_SNAPSHOT_DEVICE_T dev = (PCK600_SNAPSHOT_DEVICE_T)i;
+			PCK600_SNAPSHOT_STATUS_T status = pck600_snapshot_get_device_status(dev);
+
+			LOG_INF("active IP: %s (bit %d) qactive=%d",
+				pm_snapshot_ip_names[i], i, status);
+		}
+	}
+
+	pck600_snapshot_clear_all_device_status();
+	irq_unlock(key);
+}
+
+static void pm_snapshot_report_handler(struct k_work *work)
+{
+	bool lp_overflow = false;
+	uint32_t lp_count = pck600_system_get_wakeup_count(&lp_overflow);
+
+	LOG_INF("PM snapshot: wfi=%u  lp_enter=%u%s",
+		pm_wfi_count, lp_count, lp_overflow ? " (overflow)" : "");
+
+	pm_snapshot_log_active_ips();
+}
+
+static K_WORK_DEFINE(pm_snapshot_work, pm_snapshot_report_handler);
+
+static void pm_snapshot_timer_handler(struct k_timer *timer)
+{
+	k_work_submit(&pm_snapshot_work);
+}
+
+static K_TIMER_DEFINE(pm_snapshot_timer, pm_snapshot_timer_handler, NULL);
+
+#endif /* CONFIG_PM_DEBUG_SNAPSHOT */
+
 static inline void pm_prepare_power_gating(void)
 {
 	SCB->CPACR &= ~(0xFU << 20);
@@ -66,6 +123,9 @@ void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 
 	switch (state) {
 	case PM_STATE_SUSPEND_TO_IDLE:
+#ifdef CONFIG_PM_DEBUG_SNAPSHOT
+		pm_wfi_count++;
+#endif
 		pm_power_on_sequence();
 		break;
 	default:
@@ -94,6 +154,10 @@ void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 static int rtl87x2j_system_pm_init(void)
 {
 	pck600_system_set_dynamic_power_policy(POWER_POLICY_SYSTEM_ON_LOW_POWER);
+
+#ifdef CONFIG_PM_DEBUG_SNAPSHOT
+	k_timer_start(&pm_snapshot_timer, K_MSEC(3000), K_MSEC(3000));
+#endif
 
 	return 0;
 }
